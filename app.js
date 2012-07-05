6096023374c2635c4e5fb22c9c1e3ee37a079b98
Alex Ninneman <ninnemana@gmail.com>
2012-07-05 13:58:53 -0500

    fixed redis binding

--- app.js
+++ app.js
@@ -5,10 +5,19 @@
 
 var express = require('express'),
     auth_routes = require('./routes/auth'),
-    global_routes = require('./routes/index');
+    global_routes = require('./routes/index'),
+    redis = {};
 
 var app = module.exports = express.createServer();
 var RedisStore = require('connect-redis')(express);
+
+if(process.env.REDISTOGO_URL){
+    var rtg = require('url').parse(process.env.REDISTOGO_URL);
+    redis = require('redis').createClient(rtg.port, rtg.hostname);
+    redis.auth = rtg.split(':')[1];
+}else{
+    redis = require('redis').createClient();
+}
 
 // Configuration
 
@@ -18,7 +27,12 @@ app.configure(function(){
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
-    app.use(express.session({ secret: 'd0ughb0y', store: new RedisStore() }));
+    app.use(express.session({
+        secret: process.env.CLIENT_SECRET || 'd0ughb0y',
+        store: new RedisStore({
+            client: redis
+        })
+    }));
     app.use(require('stylus').middleware({ src: __dirname + '/public' }));
     app.use(express.static(__dirname + '/public'));
     app.use(app.router); // We have to place our router after our static directory, otherwise it won't pass
